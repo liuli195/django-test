f8d8779b725ce04efd8f90b6e78ca88e33abe07b
liu <liu@liu-PC.mshome.net>
2013-01-08 17:37:04 +0800

    1301081736-office

--- mydjango/static/js/base.js
+++ mydjango/static/js/base.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
     
-    var $subBox = $("#selected-light").find("input:checkbox");
+    var $subBox = $("#selected-light > tr");
     
+    //选中单个任务的行为
     $subBox.click(function() {
-        var id = '#' + $(this).attr("id");
-        $(id).toggleClass("info");
+        $(this).toggleClass("info");
+        
+        if ($(this).hasClass("info")) {
+        	$(this).find("input:checkbox").attr("checked", true);
+        } else {
+        	$(this).find("input:checkbox").attr("checked", false);
+        }
         
         if ($subBox.length == $("#selected-light").find('input:checkbox:checked').length) {
             $("#all").attr("checked", true);
@@ -23,6 +29,7 @@ $(document).ready(function() {
         }
     })
     
+    //选中所有任务的行为
     $("#all").click(function() {
         $("#selected-light").find('input:checkbox').attr('checked', this.checked);
         
@@ -39,6 +46,7 @@ $(document).ready(function() {
         }
     })
     
+    //如果复选框和任务默认情况下是选择的，则高色
     $('#selected-light > tr:has(:checked)').addClass("info");
     
     if ($("#selected-light").find('input:checkbox:checked').length != 0) {
@@ -47,6 +55,7 @@ $(document).ready(function() {
         })
     }
     
+    //下拉菜单使用链接提交表单
     $(".dropdown-menu").find("a").click(function() {
         var names = $(this).attr("name");
         var values = $(this).attr("value");
@@ -55,15 +64,28 @@ $(document).ready(function() {
         $("#edit_list").submit();
     })
     
-    var widths = $(".myalert").width()
-    var widths = widths / 2
+    //显示并延时20秒关闭消息提示
+    var widths = $(".myalert").width();
+    var widths = widths / 2 ;
     $(".myalert").css("margin-left", "-" + widths + "px");
     
     if ($(".alert").attr("state") == "show") {
-        $(".alert").show()
+        $(".alert").show();
         
         setTimeout(function() {
         	$(".alert").alert('close');
     	}, 20000);
     }
+    
+    //同步新增工具栏和列表的宽度
+    var widths = $("#selected-light").width()
+    $(".add_nav").css("width", widths + "px");
+    
+    //初始化弹出提示框
+    $('.myicon').popover({
+    	placement: 'bottom',
+    	content: '!优先级<br/>^开始时间',
+    	title: '智能添加快捷键',
+    	html: true,
+    })
 })
